cd766e3cb3e2d8325639b934e944ce5670afa858
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-25 10:29:49 +0700

    Compare eager and lazy filters

--- src/main/kotlin/Filters.kt
+++ src/main/kotlin/Filters.kt
@@ -1,4 +1,12 @@
-val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 fun main() {
-    println(decorations.filter { it[0] == 'p' })
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+    val newList = filtered.toList()
+    println("lazy: $newList" )
 }
