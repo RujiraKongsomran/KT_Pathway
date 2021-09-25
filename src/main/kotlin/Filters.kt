1cad259b0f3f07c7f8070f572ee4318130f0ac71
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-25 10:45:21 +0700

    map()

--- src/main/kotlin/Filters.kt
+++ src/main/kotlin/Filters.kt
@@ -1,12 +1,31 @@
 fun main() {
-    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+    val decorations = listOf("rock", "pagoda", "plastic", "alligator", "flowerpot")
 
-    val eager = decorations.filter { it[0] == 'p' }
-    println("eager: $eager")
+//    val eager = decorations.filter { it[0] == 'p' }
+//    println("eager: $eager")
+//
+//    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+//    println("filtered: $filtered")
+//
+//    val newList = filtered.toList()
+//    println("lazy: $newList" )
 
-    val filtered = decorations.asSequence().filter { it[0] == 'p' }
-    println("filtered: $filtered")
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
 
-    val newList = filtered.toList()
-    println("lazy: $newList" )
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    val lazyMap2 = decorations.asSequence().filter { it[0] == 'p' }.map {
+        println("access: $it")
+        it
+    }
+
+    println("filtered: ${lazyMap2.toList()}")
 }
+
