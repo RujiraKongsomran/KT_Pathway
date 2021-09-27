a0032ef77f4a8b2ee054883181cb56c5fa46c01e
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-27 13:04:50 +0700

    Add init blocks

--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -3,15 +3,19 @@ import example.myapp.Aquarium
 fun buildAquarium(){
     val aquarium1 = Aquarium()
     aquarium1.printSize()
+    println("-----------")
 
     val aquarium2 = Aquarium(width = 25)
     aquarium2.printSize()
+    println("-----------")
 
     val aquarium3 = Aquarium(height = 35, length = 110)
     aquarium3.printSize()
+    println("-----------")
 
     val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
     aquarium4.printSize()
+    println("-----------")
 
     val aquarium5 = Aquarium()
     aquarium5.printSize()
