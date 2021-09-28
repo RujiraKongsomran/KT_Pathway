ca05cbec35fa22ca12a33c40ac6ff4c2984465fa
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-28 14:52:08 +0700

    Learn about secondary constructors

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,6 +1,10 @@
 package example.myapp
 
 class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
 
     init {
         println("aquarium initializing")
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,26 +1,30 @@
 import example.myapp.Aquarium
 
-fun buildAquarium(){
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    println("-----------")
+fun buildAquarium() {
+//    val aquarium1 = Aquarium()
+//    aquarium1.printSize()
+//    println("-----------")
+//
+//    val aquarium2 = Aquarium(width = 25)
+//    aquarium2.printSize()
+//    println("-----------")
+//
+//    val aquarium3 = Aquarium(height = 35, length = 110)
+//    aquarium3.printSize()
+//    println("-----------")
+//
+//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+//    aquarium4.printSize()
+//    println("-----------")
+//
+//    val aquarium5 = Aquarium()
+//    aquarium5.printSize()
 
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    println("-----------")
-
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-    println("-----------")
-
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
-    println("-----------")
-
-    val aquarium5 = Aquarium()
-    aquarium5.printSize()
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 }
 
-fun main(){
+fun main() {
     buildAquarium()
 }
