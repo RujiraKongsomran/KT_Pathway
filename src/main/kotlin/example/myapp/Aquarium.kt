ca612458a5c9e42e87f2923317ce31d5265f6847
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-28 16:00:00 +0700

    Make the Aquarium class open #2

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,6 +1,6 @@
 package example.myapp
 
-open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     open var volume: Int
         get() = width * height * length / 1000
         set(value) {
